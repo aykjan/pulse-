fde36c4d94d38d6c49fd0d6593b63483db18e11f
aykjan <ayk_arzumanyan@mail.ru>
2024-02-10 14:02:26 +0300

    Slick slaider заменен на Tiny-slider для экономий памяти

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,36 @@
-$(document).ready(function () {
-  $('.carusel__inner').slick({
-    speed: 1200,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/array-right.png"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
-  });
+// $(document).ready(function () {
+//   $('.carusel__inner').slick({
+//     speed: 1200,
+//     adaptiveHeight: true,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="../icons/array-right.png"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           dots: true,
+//           arrows: false
+//         }
+//       }
+//     ]
+//   });
+// });
+
+
+const slider = tns({
+  container: '.carusel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls:false,
+  nav:false
 });
+document.querySelector('.next').addEventListener('click',()=>{
+  slider.goTo('next');
+})
+
+document.querySelector('.prev').addEventListener('click',()=>{
+  slider.goTo('prev');
+})
+
+
