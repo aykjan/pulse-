afa789bb793ae6c622a1e788c719f4dba437393d
aykjan <ayk_arzumanyan@mail.ru>
2024-02-20 17:10:15 +0300

    Добавлена валидация форм через скрипты

--- src/js/script.js
+++ src/js/script.js
@@ -17,62 +17,94 @@
 // });
 
 $(document).ready(function () {
-const slider = tns({
-  container: '.carusel__inner',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  controls:false,
-  nav:false,
-  responsive: {
-    320: {
-      gutter: 20,
-      items: 1,
-      touch:true,
-      center:true
-    },
-  }
-});
-document.querySelector('.next').addEventListener('click',()=>{
-  slider.goTo('next');
-})
-
-document.querySelector('.prev').addEventListener('click',()=>{
-  slider.goTo('prev');
-})
-
-$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-  $(this)
-    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-});
-
-
-function toggleSlide(item) {
-  $(item).each(function(i) {
-      $(this).on('click', function(e) {
-          e.preventDefault();
-          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-      })
+  const slider = tns({
+    container: '.carusel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+      320: {
+        gutter: 20,
+        items: 1,
+        touch: true,
+        center: true
+      },
+    }
   });
-};
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+  document.querySelector('.next').addEventListener('click', () => {
+    slider.goTo('next');
+  })
 
-    // modal 
-    $('[data-modal=consultation]').on('click',function(){
-      $('.overlay, #consultation').fadeIn('slow');
-    });
-    $('.modal__close').on('click',function() {
-      $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
-     
-    });
-    
-    $('.button_mini').each(function(i) {
-      $(this).on('click',function() {
-        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-        $('.overlay, #order').fadeIn('slow');
+  document.querySelector('.prev').addEventListener('click', () => {
+    slider.goTo('prev');
+  })
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
       })
+    });
+  };
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  // modal 
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+
+  });
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
     })
+  })
+
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: 'Пожалуйста введите свое имя!',
+          minlength: jQuery.validator.format("Имя должно состоять из  {0} Символов")
+        },
+        phone: "Пожалуйтса введите свой телефон",
+        email: {
+          required: "Пожалуйста введите свою почта",
+          email: "Ваша почта должна быть формата name@domain.com"
+        }
+      }
+
+
+    });
+  };
+  valideForms('#consultation-form');
+  valideForms('#consultation form');
+  valideForms('#order form');
 })
